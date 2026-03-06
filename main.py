1e08a0c5dc333949a90226ec7551b75d4480ce52
santhosh220z <santhoshsunkarasbe@gmail.com>
2026-03-06 10:18:08 +0530

    added enemy and made it's position random

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import pygame
-
+import random
 # initilize the pygame 
 pygame.init()
 #creating a display for the game 
 screen = pygame.display.set_mode((800,600))
 #setting icon and displayname for the game
 pygame.display.set_caption("Space_Invaders!")
-icon = pygame.image.load("spaceship.png")
+icon = pygame.image.load("icons_and_characters/spaceship.png")
 pygame.display.set_icon(icon)
 #player_image
-player_img = pygame.image.load("Adobe Express - file.png")
+player_img = pygame.image.load("icons_and_characters/Adobe Express - file.png")
 #default player position
 pos_x = 370
 pos_y = 480
 pos_x_cha = 0
 pos_y_cha = 0
+#enemy attribute
+enemy_img = pygame.image.load("icons_and_characters/alien.png")
+
+enemy_pos_x = random.randint(0,736)
+enemy_pos_y = random.randint(50,150)
+enemy_pos_x_cha = 0
+enemy_pos_y_cha = 0
 #defining player
 def player(pos_x,pos_y):
     screen.blit(player_img,(pos_x, pos_y))
+#defining enemy
+def enemy(enemy_pos_x,enemy_pos_y):
+    screen.blit(enemy_img, (enemy_pos_x, enemy_pos_y))
 #game loop (for the screen to not close after running completed)
 running = True
 while running:
@@ -45,4 +55,5 @@ while running:
     elif pos_x >= 736:
         pos_x = 736
     player(pos_x, pos_y)
+    enemy(enemy_pos_x,enemy_pos_y)
     pygame.display.update()
